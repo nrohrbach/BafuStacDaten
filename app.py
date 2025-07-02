eb92e7c6f6a45f65fff6ba828d698da222231e4c
nrohrbach <52918447+nrohrbach@users.noreply.github.com>
2025-07-02 07:18:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,10 +25,11 @@ dfLayers_filtered = dfLayers[~dfLayers['downloadUrl'].str.contains("https://data
 AnteilMigriert = len(dfLayers_filtered) / len(dfLayers) * 100
 AnteilMigriert = round(AnteilMigriert)
 AnteilMigriert = "{AnteilMigriert}  der BAFU Layer sind auf STAC."
+AnteilMigriert = "70% mirgierW"
 
 # App
 # Streamlit app
-st.title("BAFU Geodaten welche noch nicht in die STAC API migriert wurden")
+st.header("BAFU Geodaten welche noch nicht in die STAC API migriert wurden",divider='red')
 st.markdown("Das BAFU migriert alle Geodaten in der BGDI in die STAC API. Diese Applikation zeigt auf, welche Layer noch migriert werden müssen.")
 st.subheader("Anteil bereits migrierter Layer")
 st.badge(AnteilMigriert, color="green", width=20)
