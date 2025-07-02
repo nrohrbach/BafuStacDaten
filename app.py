6e2e01772745269ff73d98ed4a0305d50b72dff3
nrohrbach <52918447+nrohrbach@users.noreply.github.com>
2025-07-02 06:53:31 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import requests
+import pandas as pd
+import json
+import streamlit as st
+
+# Metadaten über BGDI abfragen
+url = "https://api3.geo.admin.ch/rest/services/api/MapServer?searchText=ch.bafu"
+response = requests.get(url).json()
+
+# Downloadlink und Layer ID holen
+Layers = []
+for layer in response['layers']:
+  try:
+    layerBodId = layer['layerBodId']
+    downloadUrl = layer['attributes']['downloadUrl']
+    Layers.append({'layerBodId': layerBodId, 'downloadUrl': downloadUrl})
+  except:
+    Layers.append({'layerBodId': layerBodId, 'downloadUrl': ''})
+
+# Layer Details als Dataframe speichern und filtern
+dfLayers = pd.DataFrame(Layers)
+dfLayers_filtered = dfLayers[~dfLayers['downloadUrl'].str.contains("https://data.geo.admin.ch/browser")]
+
+# App
+# Streamlit app
+st.title("BAFU Geodaten welche noch nicht in die STAC API migriert wurden")
+st.markdown("Das BAFU migriert alle Geodaten in der BGDI in die STAC API. Diese Applikation zeigt auf, welche Layer noch migriert werden müssen.")
+st.subheader("Anteil bereits migrierter Layer")
+st.badge("xyss", color="blue")
+
+st.dataframe(dfLayers_filtered)  
